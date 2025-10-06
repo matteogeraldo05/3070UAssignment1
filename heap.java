5ad03d448901c09ffc9efdc25d2745c3092e1744
ThaddeusBat <thaddeus.batruensky@ontariotechu.net>
2025-10-06 16:16:07 -0400

    completed alot from Part 4 such as maxheapify,buildmaxheap,heap-maximum,heapextractmax

--- heap.java
+++ heap.java
@@ -2,6 +2,7 @@ public class heap {
     private int[] heap;
     private int size;
     private int capacity;
+    private boolean isMaxHeap = false;
 
     public heap(int capacity) {
         this.capacity = capacity;
@@ -12,6 +13,7 @@ public class heap {
     private int parent(int i) { return (i - 1) / 2; }
     private int left(int i) { return 2 * i + 1; }
     private int right(int i) { return 2 * i + 2; }
+    private boolean checkIsMaxHeap(){return isMaxHeap;} 
 
     public void insert(int value) {
         if (size == capacity) throw new IllegalStateException("Heap is full");
@@ -31,11 +33,58 @@ public class heap {
         System.out.println();
     }
 
-    public static void main(String[] args) {
-        heap h = new heap(10);
-        h.insert(10);
-        h.insert(5);
-        h.insert(7);
-        h.printHeap();
+    public void maxHeapify(int index){
+        if (index < 0 || index > size){return;}
+        
+        int largestValue = index;
+
+        if (left(index) < size && heap[largestValue] < heap[left(index)]){
+            largestValue = left(index);
+        }
+        if (right(index) < size && heap[largestValue] < heap[right(index)]){
+            largestValue = right(index);
+        }
+
+        if (largestValue != index){
+            int temp = heap[index];
+            heap[index] = heap[largestValue];
+            heap[largestValue] = temp;
+
+            maxHeapify(largestValue);
+        }
+
+        isMaxHeap = true;
     }
+
+    public void buildMaxHeapify(){
+        int lastLeaf = (size/2) - 1;
+
+        for (int i = lastLeaf; i >= 0; i--){
+            maxHeapify(i);
+        }
+        isMaxHeap = true;
+    }
+
+    public int heapMaximum(){
+        if(checkIsMaxHeap()){return heap[0];}
+        else {
+            int largestValue = heap[0];
+            for (int heapValue : heap){
+                if (heapValue > largestValue){largestValue = heapValue;}
+            }
+            return largestValue;
+        }
+    }
+
+    public int heapExtractMax(){
+        if (size == 0){return -1;}
+        int maxVal = heap[0];
+        heap[0] = heap[size - 1];
+        size--;
+
+        maxHeapify(0);
+        return maxVal;
+    }
+
+    public static void main(String[] args) {}
 }
